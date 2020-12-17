6d8d204531cf52987e1c3dcabfd242500ab97927
tanmoytonu <tanmoyacharjee18@gmail.com>
2020-12-18 00:41:26 +0600

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,78 @@
+const defaultResult = 0;
+
+let currentResult=defaultResult;
+
+let logEntries = [];
+
+
+function getInputValue(){
+    return parseInt(userInput.value)
+}
+function btnNav(operator,firstValue,inputValue){
+    const calcDes = `${firstValue} ${operator} ${inputValue}`;
+    outputResult(currentResult,calcDes);
+}
+
+function writeToLog(
+    operationIdentifier,
+    previousResult,
+    inputNumber,
+    finalResult){
+    const logEntry={
+        operation: operationIdentifier,
+        prevResult: previousResult,
+        enteredNum: inputNumber,
+        result: finalResult,
+       }
+    logEntries.push(logEntry);
+     console.log(logEntries);
+
+}
+function calculation(calcType){
+    const getInput=getInputValue();
+    const initialResult =currentResult;
+    let mathOperator;
+    if(calcType==='add'){
+        currentResult +=getInput;
+        mathOperator= '+'
+    }
+    else if(calcType==='subtract'){
+        currentResult-=getInput;
+        mathOperator= '-'
+    }
+    else if(calcType==='multiply'){
+        currentResult*=getInput;
+        mathOperator= '*'
+    }
+    else if(calcType==='divide'){
+        currentResult/=getInput;
+        mathOperator= '/'
+    }
+    writeToLog(calcType,initialResult,getInput,currentResult);
+    btnNav(mathOperator,initialResult,getInput);
+}
+
+function add(){
+   
+     calculation('add');
+    
+}
+function subtract(){
+    calculation('subtract');
+
+}
+function multiply(){
+    calculation('multiply')
+}
+function divide(){
+    calculation('divide');
+}
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
+
+
+
+
+
